daad73e2893191eff5328fb65b56bf17674c6191
stigarm1 <stigarm1@gmail.com>
2016-09-21 23:03:12 +0200

    Create cpep.cpp

---
+++ cpep.cpp
@@ -0,0 +1,12 @@
+// SEN KOLL MED const i min(..), cppcon sutter
+#include ....
+int min(int x,int y)
+{return (x<y):x?y;}
+
+int main(void)
+{ int& a=5;
+  int& b=7;
+  int& f=min(a,b);
+  cout<<"f="<<f;
+  return 0;
+}
