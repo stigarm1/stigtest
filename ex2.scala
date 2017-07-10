e390ffff215393e70d230e0927708b6a53703c8c
strm9693 <stigarm1@users.noreply.github.com>
2017-07-10 15:57:37 +0200

    Create ex2.scala

---
+++ ex2.scala
@@ -0,0 +1,14 @@
+object Demo {
+   def main(args: Array[String]) {
+      println(matchTest(2))
+      println(dos(3))
+   }
+   
+   def matchTest(x: Int): String = x match {
+      case 1 => "one"
+      case 2 => "two"
+      case 3 => "three"
+      case _ => "many"
+   }
+   def dos(x:Int):Int=2*x
+}
