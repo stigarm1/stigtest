611743981f125c4ad81fcf4ad471ec04f36b35b7
strm9693 <stigarm1@users.noreply.github.com>
2018-05-11 16:21:35 +0200

    Update ex2.scala

--- ex2.scala
+++ ex2.scala
@@ -1,10 +1,10 @@
-object Demo {
-   def main(args: Array[String]) {
+object ex2 {
+   def main(args:Array[String]) {
       println(matchTest(2))
       println(dos(3))
    }
    
-   def matchTest(x: Int): String = x match {
+   def matchTest(x:Int):String = x match{
       case 1 => "one"
       case 2 => "two"
       case 3 => "three"
